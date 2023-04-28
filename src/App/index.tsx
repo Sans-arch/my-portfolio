8b626b998964ec8fe08e80383eb63295eddbf5ef
Santiago Negreira <santiago.negreira@gesplan.com.br>
2023-04-28 12:16:19 -0300

    Clean the project, add TailwindCSS

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,20 +1,8 @@
-import { ThemeProvider } from "styled-components"
-import { Header } from "../components/Header"
-import { ProfileSection } from "../components/ProfileSection"
-import { GlobalStyle } from "../styles/global"
-
-import { defaultTheme } from "../styles/themes/default"
-import { ContentContainer } from "./styles"
-
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Header />
-      <ContentContainer>
-        <ProfileSection />
-      </ContentContainer>
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+
+    </>
   )
 }
 
--- src/App/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ContentContainer = styled.main`
-  /* border: 2px solid red; */
-  width: 80%;
-  margin: 0 auto;
-`
