755c76f040e5a9218436f65ba65a581ecd33942e
Santiago Fleitas <sansfleitas@gmail.com>
2024-04-07 14:29:37 -0300

    Update profile description

--- src/App/components/ServicesText.tsx
+++ src/App/components/ServicesText.tsx
@@ -1,15 +1,30 @@
 export function ServicesText() {
   return (
     <div>
-      <h3 className='text-3xl py-1 dark:text-teal-500'>
-        Sobre mim
-      </h3>
-      <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
-        Desenvolvedor Full-stack com experiência em Java, React, Node, JavaScript, TypeScript.
-      </p>
-      <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
-        Apaixonado pelo desenvolvimento de software. Trabalho há 3 anos na área de desenvolvimento, com estudo constante sobre diversas tecnologias. Cursando Engenharia de Software no Centro Universitário Católica de Santa Catarina.
+      <h3 className="text-3xl py-1 dark:text-teal-500">Sobre mim</h3>
+      <p className="text-start py-4 leading-8 text-gray-800 dark:text-white">
+        Olá! Eu sou Santiago, um entusiasta do desenvolvimento de software apaixonado por criar soluções inovadoras e
+        funcionais que fazem a diferença. Com uma base sólida em Java, JavaScript, React, Node e 4 anos de experiência
+        na indústria, estou constantemente buscando maneiras de elevar o padrão e entregar resultados excepcionais.
+        Minha jornada no desenvolvimento de software começou em 2020, quando fui introduzido ao fascinante mundo da
+        programação. Desde então, tenho trabalhado em uma variedade de projetos desafiadores, desde aplicativos web
+        dinâmicos até sistemas de gerenciamento de banco de dados robustos.
+        <br />
+        Ao longo dos anos, adquiri experiência significativa em Desenvolvimento Front-end, Back-end. Minha abordagem é
+        centrada no usuário, garantindo que minhas soluções sejam intuitivas, eficientes e agradáveis de usar. Além de
+        minhas habilidades técnicas, sou um comunicador eficaz e colaborador em equipe. Acredito firmemente na
+        importância da comunicação clara e da colaboração para o sucesso de qualquer projeto de desenvolvimento de
+        software. Trabalho bem tanto de forma independente quanto em equipe, e estou sempre aberto a novas ideias e
+        perspectivas. Este site de portfólio serve como uma vitrine do meu trabalho e realizações até o momento. Aqui
+        você encontrará uma seleção de projetos nos quais trabalhei, demonstrando minha habilidade em transformar
+        conceitos em realidade digital.
+        <br />
+        Estou sempre em busca de novos desafios e oportunidades emocionantes para aplicar minhas habilidades e continuar
+        crescendo como profissional de desenvolvimento de software. Obrigado por visitar meu site de portfólio e por
+        explorar um pouco mais sobre mim e meu trabalho. Se você estiver interessado em colaborar em projetos futuros ou
+        apenas deseja trocar ideias, fique à vontade para entrar em contato. Estou ansioso para conectar-me e contribuir
+        para projetos incríveis no futuro!
       </p>
     </div>
-  )
+  );
 }
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,28 +1,25 @@
-import '../styles/globals.css'
-import { useState } from 'react'
+import "../styles/globals.css";
+import { useState } from "react";
 
-import { Header } from './components/Header';
-import { Introduction } from './components/Introduction';
-import { SocialMedia } from './components/SocialMedia';
-import { MainImage } from './components/MainImage';
-import { Services } from './components/Services';
-import { Portfolio } from './components/Portfolio';
+import { Header } from "./components/Header";
+import { Introduction } from "./components/Introduction";
+import { SocialMedia } from "./components/SocialMedia";
+import { MainImage } from "./components/MainImage";
+import { Services } from "./components/Services";
+import { Portfolio } from "./components/Portfolio";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true)
+  const [darkMode, setDarkMode] = useState(true);
 
   function handleChangeTheme() {
-    setDarkMode(!darkMode)
+    setDarkMode(!darkMode);
   }
 
   return (
     <div className={darkMode ? "dark" : ""}>
-      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
-          <Header
-            isDarkModeEnabled={darkMode}
-            onChangeTheme={handleChangeTheme}
-          />
+          <Header isDarkModeEnabled={darkMode} onChangeTheme={handleChangeTheme} />
           <Introduction />
           <SocialMedia />
           <MainImage />
@@ -31,7 +28,7 @@ function App() {
         <Portfolio />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
